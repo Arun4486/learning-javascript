b876a508f797ca0d8190995fcc25ce792c0f1b1b
Arun Katiyar <130146424+Arun4486@users.noreply.github.com>
2024-09-21 12:28:45 +0000

    container confige done

---
+++ basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict" // for engine to treat all code as newer version of js
+// alert(2+3) it can't be directly used in nodejs
+
+// code should be readable
+
+//types
+//01 number range= <= 2^53   , bigInt  for larger no
+let num = 1
+
+//02 string
+let name = "str"
+
+//03 boolean   true/false
+let isTrue = false
+
+// null standalone value
+let temp = null; 
+let a = "" //not an example bcz it's string
+
+//undifined not assigned
+let number;
+
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); //undifined
