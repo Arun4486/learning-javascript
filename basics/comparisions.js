6bde2d08aec6e67b90f2a6f275c69c6235045460
Arun Katiyar <130146424+Arun4486@users.noreply.github.com>
2024-10-01 09:27:56 +0000

    datatype comparisions

---
+++ basics/comparisions.js
@@ -0,0 +1,20 @@
+// <
+// <= , >=
+// == 
+// !=
+
+//console.log("2" > 0); //true
+
+// console.log(null > 0);  //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true     avoid such comparisions
+
+// for undifined always false in same cases
+// comparision and equality check works differently in js
+
+// === strict check => checks value and its datatype
+// console.log(2 == "2"); //true
+// console.log("2" === 2); //false
+
+
+
