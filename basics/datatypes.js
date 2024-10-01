b62a2a0284e6f0cf6da8be4421abf9cdc2b40887
Arun Katiyar <130146424+Arun4486@users.noreply.github.com>
2024-10-01 09:15:25 +0000

    datatypes and operations

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,31 +1,69 @@
-"use strict" // for engine to treat all code as newer version of js
-// alert(2+3) it can't be directly used in nodejs
+// "use strict" // for engine to treat all code as newer version of js
+// // alert(2+3) it can't be directly used in nodejs
 
-// code should be readable
+// // code should be readable
 
-//types
-//01 number range= <= 2^53   , bigInt  for larger no
-let num = 1
+// //types
+// //01 number range= <= 2^53   , bigInt  for larger no
+// let num = 1
 
-//02 string
-let name = "str"
+// //02 string
+// let name = "str"
 
-//03 boolean   true/false
-let isTrue = false
+// //03 boolean   true/false
+// let isTrue = false
 
-// null standalone value
-let temp = null; 
-let a = "" //not an example bcz it's string
+// // null standalone value
+// let temp = null; 
+// let a = "" //not an example bcz it's string
 
-//undifined not assigned
-let number;
+// //undifined not assigned
+// let number;
 
-// symbol => unique
+// // symbol => unique
 
-// object
+// // object
 
-console.log(typeof null); // object
+// console.log(typeof null); // object
 
-console.log(typeof undefined); //undifined
+// console.log(typeof undefined); //undifined
 
-//changed for the description
+// //changed for the description
+
+
+//*******************== operations ==*******************/
+
+let str1 = "2"
+let str2 = "2"
+//console.log(str1+str2);   //22
+
+//console.log("1" + 2); //12
+//console.log(1 + "2"); //12
+
+//console.log("1" + 2 + 2); //122
+
+//console.log(1 + 2 + "2"); //32
+
+// use more paranthesis for clarity and refer to ecma script for doubts
+
+//console.log(+true); // 1
+//console.log(+""); //0
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let count = 100
+//console.log(count++); //101
+//console.log(++count); //101  refer to prefix and suffix js mdn
+let x = 3;
+const y = x++;
+console.log(x,y);
+
+// x is 4; y is 3
+
+let x2 = 3;
+const y2 = ++x2;
+console.log(x2,y2);
+//
+// x2 is 4; y2 is 4
+
