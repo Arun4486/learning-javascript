0dc3286760e325078f30f88bfd1172273d450043
Arun Katiyar <130146424+Arun4486@users.noreply.github.com>
2024-12-08 12:08:32 +0000

    notes on datatypes

---
+++ datatypes-summary.js
@@ -0,0 +1,17 @@
+// Premitive (call by value) 
+//----------------------------
+// string, number, Boolean, null, undefind, symbol, bigInt
+
+// Js is dynamically typed language 
+const a = 100 // no datatype declared
+
+// symbol
+const id = Symbol('123')
+
+const id2 = Symbol('123')
+
+console.log(id == id2); 
+
+// Non-Premitive (call by reference)
+//-----------------------------
+// array, objects, functions
