cb09496335684e6eb6821a9b50755784eb4114c6
Arun Katiyar <130146424+Arun4486@users.noreply.github.com>
2024-12-17 07:19:18 +0000

    memory managenent for duff datatypes

--- basics/datatypes-summary.js
+++ basics/datatypes-summary.js
@@ -10,7 +10,7 @@ const id = Symbol('123')
 
 const id2 = Symbol('123')
 
-console.log(id === id2);  //false
+//console.log(id === id2);  //false
 
 const BigNum = 12345678n; // to store larger numbers adding n in the last makes it BigInt 
 
@@ -26,5 +26,35 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("hello world");
+    //console.log("hello world");
 }
+//=============MEMORY MANAGEMENT IN JS============================
+
+// for Primitive datatypes (STACK)  memory is used, we get a copy of declared variable
+
+// for Non-Peimitive (HEAP) is  used , we get reference of the original value
+
+let myName = "Arun";
+let anotherName = myName; // it gives another name a copy of myName
+
+// example for primitive stack memory
+//console.log(anotherName); // Arun
+
+anotherName = "Varun";
+//console.log(anotherName); //varun
+//console.log(myName); //Arun , no change in its original value
+
+//example for non-primitive heap memory
+
+let userOne = {
+    email:"user@gmail.com",
+    id:"user@ybl"
+}
+let userTwo = userOne
+//console.log(userOne.email); //user@gmail.com
+//console.log(userTwo.email); //user@gmail.com
+
+userTwo.email = "arun@gmail.com"
+console.log(userOne.email); //arun@gmail.com , it gets changed in both of them bcoz of referencing
+console.log(userTwo.email); //arun@gmail.com
+
