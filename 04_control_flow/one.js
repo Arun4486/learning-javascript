a91024e12252ee7bc165d0727bc0e7f4bdf24c6d
Arun Katiyar <130146424+Arun4486@users.noreply.github.com>
2025-04-14 07:15:14 +0000

    basis if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,36 @@
+// IF
+const isUserLoggedIn = true;
+if (isUserLoggedIn) {
+    //console.log("Logged in "); // logged in
+    
+}
+
+// COMPARISION operators
+// console.log(3!=2); // true
+
+// strict check
+// console.log(3==="3"); // false 
+// console.log(3!=="3"); // true
+
+// implicite scope => one line scope without '{}' or we can also write multiple lines of code by using comma and ';'
+// let n =5 ;
+// if(n<10) console.log("yes"), // yes
+// console.log("no"); // no
+
+// Ladder if else
+
+// const balance = 1000;
+// if (balance < 500){
+//     console.log("less than 500");
+    
+// }else if(balance<750){
+//     console.log("less than 750");
+    
+// }else if(balance<1000){
+//     console.log("less than 1000");
+    
+// }else{
+//     console.log("more than 1000");
+    
+// }
+
