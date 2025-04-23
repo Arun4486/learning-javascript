634cd48b75564b41dd3d82b6c74e09ce3c9653d5
Arun Katiyar <130146424+Arun4486@users.noreply.github.com>
2025-04-23 10:40:40 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,31 @@
+// while looop
+
+// while (condition) {
+    // code logic
+    // updation
+// }
+let i=0;
+while(i<=10){
+    //console.log(`value of i is ${i}`);
+    i++;
+}
+
+let myArr = ["flash", "batman", "superman"];
+let arr = 0;
+while(arr<myArr.length){
+    //console.log(`value is ${myArr[arr]}`);
+    arr++;
+}
+
+// do while
+
+// do {
+//     // logic
+//     //updation
+// } while (condition);
+
+let score = 1;
+do {
+    //console.log(`value is ${score}`);
+    score++;
+} while (score<=10); // till 10, but even if the condition not followed the 'do' block runs atleast once for sure
